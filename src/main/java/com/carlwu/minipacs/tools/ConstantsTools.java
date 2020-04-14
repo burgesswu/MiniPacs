44b7e6fc1774aa2de82bc738266266e136877299
burgess <wurui_0@163.com>
2020-04-14 14:18:12 +0800

    init

--- src/main/java/com/carlwu/minipacs/tools/ConstantsTools.java
+++ src/main/java/com/carlwu/minipacs/tools/ConstantsTools.java
@@ -30,40 +30,40 @@ public class ConstantsTools {
     /**
      * xpath
      */
-    public final static String XPATH_LAST_SYNC_TIME = "//weSync/status/lastSyncTime";
-    public final static String XPATH_LAST_KEEP_TIME = "//weSync/status/lastKeepTime";
-    public final static String XPATH_SUCCESS_TIME = "//weSync/status/successTime";
-    public final static String XPATH_FAIL_TIME = "//weSync/status/failTime";
+    public final static String XPATH_LAST_SYNC_TIME = "//root/status/lastSyncTime";
+    public final static String XPATH_LAST_KEEP_TIME = "//root/status/lastKeepTime";
+    public final static String XPATH_SUCCESS_TIME = "//root/status/successTime";
+    public final static String XPATH_FAIL_TIME = "//root/status/failTime";
 
-    public final static String XPATH_TYPE_FROM = "//weSync/database/from/type";
-    public final static String XPATH_HOST_FROM = "//weSync/database/from/host";
-    public final static String XPATH_NAME_FROM = "//weSync/database/from/name";
-    public final static String XPATH_USER_FROM = "//weSync/database/from/user";
-    public final static String XPATH_PASSWORD_FROM = "//weSync/database/from/password";
+    public final static String XPATH_TYPE_FROM = "//root/database/from/type";
+    public final static String XPATH_HOST_FROM = "//root/database/from/host";
+    public final static String XPATH_NAME_FROM = "//root/database/from/name";
+    public final static String XPATH_USER_FROM = "//root/database/from/user";
+    public final static String XPATH_PASSWORD_FROM = "//root/database/from/password";
 
 
-    public final static String XPATH_AUTO_BAK = "//weSync/setting/autoBak";
-    public final static String XPATH_DEBUG_MODE = "//weSync/setting/debugMode";
-    public final static String XPATH_STRICT_MODE = "//weSync/setting/strictMode";
-    public final static String XPATH_MYSQL_PATH = "//weSync/setting/mysqlPath";
-    public final static String XPATH_PRODUCT_NAME = "//weSync/setting/productname";
+    public final static String XPATH_AUTO_BAK = "//root/setting/autoBak";
+    public final static String XPATH_DEBUG_MODE = "//root/setting/debugMode";
+    public final static String XPATH_STRICT_MODE = "//root/setting/strictMode";
+    public final static String XPATH_MYSQL_PATH = "//root/setting/mysqlPath";
+    public final static String XPATH_PRODUCT_NAME = "//root/setting/productname";
 
-    public final static String XPATH_POSITION_CODE = "//weSync/increase/POSITION_CODE";
+    public final static String XPATH_POSITION_CODE = "//root/increase/POSITION_CODE";
 
     /**
      * 日志文件 路径
      */
     public final static String PATH_LOG = UiConsts.CURRENT_DIR + File.separator + "log" + File.separator + "log.log";
-    public static final String XPATH_LOGIN_NAME = "//weSync/login/USER_NAME";
-    public static final String XPATH_LOGIN_PASS = "//weSync/login/USER_PASS";
+    public static final String XPATH_LOGIN_NAME = "//root/login/USER_NAME";
+    public static final String XPATH_LOGIN_PASS = "//root/login/USER_PASS";
 
-    public static final String XPATH_BASE_URL = "//weSync/base/URL";
-    public static final String XPATH_LOCAL_PACS_AETITLE = "//weSync/base/LOCAL_PACS_AETITLE";
-    public static final String XPATH_LOCAL_PACS_PORT = "//weSync/base/LOCAL_PACS_PORT";
-    public static final String XPATH_REMOTE_PACS_PORT = "//weSync/base/REMOTE_PACS_PORT";
-    public static final String XPATH_REMOTE_PACS_URL = "//weSync/base/REMOTE_PACS_URL";
-    public static final String XPATH_REMOTE_PACS_AETITLE = "//weSync/base/REMOTE_PACS_AETITLE";
-    public static final String XPATH_LOCAL_DICOM_PATH = "//weSync/base/LOCAL_DICOM_PATH";
-    public static final String XPATH_LOGIN_TOKEN = "//weSync/login/TOKEN";
-    public static final String XPATH_UPLOAD_MODE = "//weSync/base/UPLOAD_MODE";
+    public static final String XPATH_BASE_URL = "//root/base/URL";
+    public static final String XPATH_LOCAL_PACS_AETITLE = "//root/base/LOCAL_PACS_AETITLE";
+    public static final String XPATH_LOCAL_PACS_PORT = "//root/base/LOCAL_PACS_PORT";
+    public static final String XPATH_REMOTE_PACS_PORT = "//root/base/REMOTE_PACS_PORT";
+    public static final String XPATH_REMOTE_PACS_URL = "//root/base/REMOTE_PACS_URL";
+    public static final String XPATH_REMOTE_PACS_AETITLE = "//root/base/REMOTE_PACS_AETITLE";
+    public static final String XPATH_LOCAL_DICOM_PATH = "//root/base/LOCAL_DICOM_PATH";
+    public static final String XPATH_LOGIN_TOKEN = "//root/login/TOKEN";
+    public static final String XPATH_UPLOAD_MODE = "//root/base/UPLOAD_MODE";
 }
--- src/main/java/com/carlwu/minipacs/tools/DcmReader.java
+++ src/main/java/com/carlwu/minipacs/tools/DcmReader.java
@@ -14,7 +14,10 @@ public class DcmReader {
         try {
             DicomInputStream dis = new DicomInputStream(new File(filePath));
             Attributes dataSet = dis.readDataset(-1, Tag.PixelData);
+
             System.out.println(dataSet.getString(Tag.PatientID));
+            System.out.println(dataSet.getString(Tag.PatientName));
+            System.out.println(dataSet.getString(Tag.SpecificCharacterSet));
             return dataSet;
         } catch (IOException e) {
             e.printStackTrace();
@@ -24,7 +27,8 @@ public class DcmReader {
 
 
     public static void main(String[] args) {
-        DcmReader.readDcmFile("/home/burgess/dicom/lhf/154974990/AT000001.1145 122.dcm");
+        DcmReader.readDcmFile("/home/burgess/pacs/dicom2/201803504/1.2.840.113619.2.278.3.839569194.406.1566954975.113/1.2.840.113619.2.80.4203691058.20895.1567046977.1.4.1/1.2.840.113619.2.80.4203691058.20895.1567046977.2.dcm");
+//        DcmReader.readDcmFile("/home/burgess/pacs/dicom2/3914A955.dcm");
     }
 
 }
